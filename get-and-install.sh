bb285a8613f2ef91eeab45d1d0d1cc73a6b01aca
InvraNet <invranet@InvraNets-MacBook-Air.local>
2024-05-11 22:52:44 +1000

    edit: change to be part 1 of automation process.

--- get-and-install.sh
+++ get-and-install.sh
@@ -1,19 +1,19 @@
 #! /bin/zsh
 
 if [[ $(uname -m) == 'arm64' ]]; then
-  curl -O https://cdxn.invra.net/gh/gh_2.49.1_macOS_arm64.tar.xz && unxz -vf gh_2.49.1_macOS_arm64.tar.xz && tar -xvf gh_2.49.1_macOS_arm64.tar
-  exec gh_2.49.1_macOS_arm64/bin/gh repo clone InvraNet/macos-installer && cd ./macos-installer
-  exit 1
+  curl -O https://cdxn.invra.net/gh/gh_2.49.1_macOS_arm64.tar.xz && unxz -vf gh_2.49.1_macOS_arm64.tar.xz && tar -xvf gh_2.49.1_macOS_arm64.tar && \
+  gh_2.49.1_macOS_arm64/bin/gh repo clone InvraNet/macos-installer 
+  echo 'Cleaning up before we continue...'
+  rm gh_2.49.1_macOS_arm64.tar
+  rm -rf gh_2.49.1_macOS_arm64
+  cd ./macos-installer
 else
-  curl -O https://cdxn.invra.net/gh/gh_2.49.1_macOS_amd64.tar.xz && unxz -vf gh_2.49.1_macOS_amd64.tar.xz && tar -xvf gh_2.49.1_macOS_amd64.tar
-  exec gh_2.49.1_macOS_amd64/bin/gh repo clone InvraNet/macos-installer && cd ./macos-installer
-  exit 1
-fi
-then
-if command -v cargo &> /dev/null
-then
-  cargo run
-  exit 1
+  curl -O https://cdxn.invra.net/gh/gh_2.49.1_macOS_amd64.tar.xz && unxz -vf gh_2.49.1_macOS_amd64.tar.xz && tar -xvf gh_2.49.1_macOS_amd64.tar && \
+  gh_2.49.1_macOS_amd64/bin/gh repo clone InvraNet/macos-installer 
+  echo 'Cleaning up before we continue...'
+  rm gh_2.49.1_macOS_amd64.tar
+  rm -rf gh_2.49.1_macOS_amd64
+  cd ./macos-installer
 fi
 
-curl https://sh.rustup.rs -sSf | sh
+sh install.sh
