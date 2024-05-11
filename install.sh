7f48faf540882c320d50371ed5ae834686008ee8
InvraNet <invranet@InvraNets-MacBook-Air.local>
2024-05-11 22:53:29 +1000

    add: part 2 control for fully automated script.

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #! /bin/zsh
 
-if command -v rustc &> /dev/null
-then
-  cargo run
-  exit 1
+if [[ -f "../get-and-install.sh" ]]; then
+  rm -vf "../get-and-install.sh"
+  echo 'Finished cleaning up.'
 fi
-curl https://sh.rustup.rs -sSf | sh
+
+if ! command -v cargo &> /dev/null; then
+  curl https://sh.rustup.rs -sSf | sh
+else
+  cargo build && cargo run
+fi
