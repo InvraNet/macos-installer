97b532a01c1626c1ae65ae98e139d8195acd3086
InvraNet <invranet@InvraNets-MacBook-Air.local>
2024-05-12 13:52:42 +1000

    fix: unxz doesnt exist on vanilla install.

--- get-and-install.sh
+++ get-and-install.sh
@@ -1,14 +1,14 @@
 #! /bin/zsh
 
 if [[ $(uname -m) == 'arm64' ]]; then
-  curl -O https://cdxn.invra.net/gh/gh_2.49.1_macOS_arm64.tar.xz && tar -xvf gh_2.49.1_macOS_arm64.tar && \
+  curl -O https://cdxn.invra.net/gh/gh_2.49.1_macOS_arm64.tar.xz && tar -xvf gh_2.49.1_macOS_arm64.tar.xz && \
   gh_2.49.1_macOS_arm64/bin/gh repo clone InvraNet/macos-installer 
   echo 'Cleaning up before we continue...'
   rm gh_2.49.1_macOS_arm64.tar.xz
   rm -rf gh_2.49.1_macOS_arm64
   cd ./macos-installer
 else
-  curl -O https://cdxn.invra.net/gh/gh_2.49.1_macOS_amd64.tar.xz && tar -xvf gh_2.49.1_macOS_amd64.tar && \
+  curl -O https://cdxn.invra.net/gh/gh_2.49.1_macOS_amd64.tar.xz && tar -xvf gh_2.49.1_macOS_amd64.tar.xz && \
   gh_2.49.1_macOS_amd64/bin/gh repo clone InvraNet/macos-installer 
   echo 'Cleaning up before we continue...'
   rm gh_2.49.1_macOS_amd64.tar.xz
